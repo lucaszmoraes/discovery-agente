a932efc49dad319da3ed36858486d2d2542ae70d
Lucas Moraes <lucaszmoraes@gmail.com>
2026-05-28 13:07:14 -0300

    feat: endpoint /slack/mensagem

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import os
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# Modelo que define o formato do JSON que o n8n vai mandar
+class MensagemSlack(BaseModel):
+    texto: str
 
 @app.get("/")
 def raiz():
     return {"status": "Discovery Agente no ar"}
+
+@app.post("/slack/mensagem")
+def receber_mensagem(mensagem: MensagemSlack):
+    return {"resposta": f"Recebi sua mensagem: {mensagem.texto}"}
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
